94f0b6716eac3e53b22137155d25701f9e6b1a39
Jim <dujieming1@gmail.com>
2025-11-01 19:02:50 -0400

    added pycache to gitignore

--- housing_budget.py
+++ housing_budget.py
@@ -1,4 +1,4 @@
 from housing_model import train_evaluate_and_predict
 
-predicted_df = train_evaluate_and_predict()
+future_df = train_evaluate_and_predict()
 
--- housing_model.py
+++ housing_model.py
@@ -66,5 +66,4 @@ def train_evaluate_and_predict():
 
 future_df = train_evaluate_and_predict()
 
-print(future_df)
 
