fdca06fb5c633764982115865ed5fe9360433fa5
Ryan Liu <ryanliuqc@gmail.com>
2025-11-02 10:14:48 -0500

    still problems

--- backend/app.py
+++ backend/app.py
@@ -7,31 +7,41 @@ CORS(app) # allow react to access
 
 @app.route('/user_info', methods=['GET','POST'])
 def get_data():
+    print("METHOD:", request.method)
+    print("HEADERS:", dict(request.headers))
+    print("RAW BODY:", request.get_data(as_text=True))
     if request.method == 'POST':
+        print("IS JSON?:", request.is_json)
         data = request.get_json()
+        print("PARSED JSON:", data)
         if not data:
             return jsonify({"error": "No JSON received"}), 400
+        return jsonify({"status": "success", "received": data})
     else:  # GET
         # Use query parameters for browser GET
         data = request.args.to_dict()
+        print("QUERY PARAMS:", data)
+        
+        # data = {
+        #     "name": data.get('name'),
+        #     "address": data.get('address'),
+        #     "school": data.get('school'),
+        #     "year": data.get('year'),
+        #     "program": data.get('program'),
+        #     "neighbourhood": data.get('neighbourhood'),
+        #     "housingType": data.get('housingType'),  # remove the trailing comma from your original code
+        #     "transport": data.get('transport'),
+        #     "eat_out": data.get('eat_out'),
+        #     "grocery_stores": data.get('grocery_stores'),
+        #     "grocery_budget": data.get('grocery_budget')
+        #     }
+        print(type(data))
 
-    user_data = {
-    "name": data.get('name'),
-    "address": data.get('address'),
-    "school": data.get('school'),
-    "year": data.get('year'),
-    "program": data.get('program'),
-    "neighbourhood": data.get('neighbourhood'),
-    "housingType": data.get('housingType'),  # remove the trailing comma from your original code
-    "transport": data.get('transport'),
-    "eat_out": data.get('eat_out'),
-    "grocery_stores": data.get('grocery_stores'),
-    "grocery_budget": data.get('grocery_budget')
-    }
-    print(user_data.get('school'))
+
+    #print(user_data.get('school'))
     return jsonify({
         "message": "User info received successfully!",
-        "received": user_data
+        "received": data
     }), 200
 
 # @app.route('/user_info', methods=['GET'])
